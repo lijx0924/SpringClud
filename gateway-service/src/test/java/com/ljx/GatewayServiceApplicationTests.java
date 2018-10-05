d69244148c25177999655dcfeba85c1896531734
lijiaxu <23456>
2018-10-06 00:00:47 +0800

    代码提交

--- gateway-service/src/test/java/com/ljx/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/ljx/GatewayServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConfigClientApplicationTests {
+public class GatewayServiceApplicationTests {
 
     @Test
     public void contextLoads() {
--- user-service/src/main/java/com/ljx/controller/UserController.java
+++ user-service/src/main/java/com/ljx/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.ljx.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author lijx
  * @Title: ${file_name}
@@ -7,5 +11,11 @@ package com.ljx.controller;
  * @Description: ${todo}
  * @date 2018/10/4下午4:02
  */
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @GetMapping("/hi")
+    public String hi(){
+        return "I'm jerry";
+    }
 }
