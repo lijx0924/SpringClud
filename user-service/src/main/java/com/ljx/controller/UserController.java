79800454324c13ff5dc2c026a48a870a9509c819
lijiaxu <23456>
2018-10-05 23:51:10 +0800

    代码提交

---
+++ user-service/src/main/java/com/ljx/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.ljx.controller;
+
+/**
+ * @author lijx
+ * @Title: ${file_name}
+ * @Package ${package_name}
+ * @Description: ${todo}
+ * @date 2018/10/4下午4:02
+ */
+public class UserController {
+}
